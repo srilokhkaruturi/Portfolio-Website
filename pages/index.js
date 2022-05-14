59d56460330a8a4f092eba361e2eb7bce97e2cf0
Srilokh Karuturi <75606167+sai-k02@users.noreply.github.com>
2022-05-13 23:32:09 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   }
   return(
     <>
-        <button onClick={() => signIn("github", {callbackUrl: 'https://portfolio-website-indol.vercel.app/'})}>Sign in with Github</button>
+        <button onClick={() => signIn("github")}>Sign in with Github</button>
         <Header />
         </>
   )
