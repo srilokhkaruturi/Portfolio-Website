6c8f619a0c21110c2bfcfa92f1501a86a51c60e6
sai-blocktrace <srilokh.karuturi@gmail.com>
2022-05-17 17:29:26 -0500

    change get github info

--- pages/api/auth/getGithubInfo.js
+++ pages/api/auth/getGithubInfo.js
@@ -3,8 +3,28 @@ import axios from 'axios'
 import nc from "next-connect"
 import cors from "cors";
 
-const handler = nc().use(cors()).get(githubInformation())
+const handler = nc()
+  .use(cors())
+  .get(async (req,res) => {
+    
+    const token = await getToken({req});
 
+    if(!token){
+        res.status(400).json({"success":"failure"})
+        return;
+    }
+  
+    const authHeader = {Authorization:'token ' + token.accessToken}
+    const url = 'https://api.github.com/user'
+  
+    let response = await axios.get(url, {headers : authHeader});
+    let githubInformation = response.data;
+    res.status(200).json(githubInformation);
+    return;
+  
+  
+  })
+/*
 async function githubInformation(req, res) {
   // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
   // const token = await getToken({ req })
@@ -23,7 +43,7 @@ async function githubInformation(req, res) {
   res.status(200).json(githubInformation);
   return;
 }
-
+*/
 
 
 export default handler;
