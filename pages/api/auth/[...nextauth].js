94c87dc64ea036bb1d4c390c5a9636f011cab927
sai-blocktrace <srilokh.karuturi@gmail.com>
2022-05-17 13:59:50 -0500

    add jwt callback to [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,16 @@ export default NextAuth({
             clientSecret : process.env.GITHUB_CLIENT_SECRET,
             
         }),
+    callbacks: {
+        async jwt({ token, account }) {
+        // Persist the OAuth access_token to the token right after signin
+        if (account) {
+            token.accessToken = account.access_token
+        }
+        return token
+        }
+        
+    }
     ],
     debug:true,
 });
--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ export default function Home() {
       
         <>
         <Header />
-        
         <Skeleton variant="rectangular" animation="wave" height={1000}/>
 
         </>
