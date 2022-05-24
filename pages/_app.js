ca47867ecdae8b6999a0a984be05a5963b3140f6
sai-blocktrace <srilokh.karuturi@gmail.com>
2022-05-23 23:59:32 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@
 File: _app.js 
 Description: This file is the root start of our program. We simple allow a sessionprovider to allow a user's session throughout the entire program
 */
-
+ 
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 
